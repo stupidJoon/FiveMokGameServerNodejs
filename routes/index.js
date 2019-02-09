2ecc1dd2b260f4bd6d4171da485d12e51f8cb4d1
stupidJoon <junsang.yu3@gmail.com>
2019-02-09 18:41:54 +0900

    Asdf

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/chat', (req, res) => {
-  res.sendFile('chat.html', { root: path.join(__dirname, '../public/html') });
-});
-
 module.exports = router;
